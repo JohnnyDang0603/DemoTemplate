84c4dd181fa464949ce6b1f54a637f40e47f07f1
Johnny Dang <johnny.dang.goldenowl@gmail.com>
2022-11-10 16:30:44 +0700

    Update code js

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,7 @@
 let slideindex = 0;
 let slidess = document.getElementsByClassName('testmonial-al');
-showautoslide();
 ///auto
-function showautoslide() {
+const showautoslide = () => {
   for (i = 0; i < slidess.length; i++) {
     slidess[i].style.display = 'none';
   }
@@ -12,41 +11,63 @@ function showautoslide() {
   }
   slidess[slideindex - 1].style.display = 'block';
   setTimeout(showautoslide, 10000);
-}
+};
 ///noauto
-function plusSlides(n) {
+const plusSlides = (n) => {
   showslide((slideindex += n));
-}
-function showslide(n) {
-  let i;
+};
+
+const showslide = (n) => {
   if (n > slidess.length) {
     slideindex = 1;
   }
   if (n < 1) {
     slideindex = slidess.length;
   }
-  for (i = 0; i < slidess.length; i++) {
+  for (let i = 0; i < slidess.length; i++) {
     slidess[i].style.display = 'none';
   }
   slidess[slideindex - 1].style.display = 'block';
-}
+};
+showautoslide();
 
+//////////////
 let btn = document.getElementById('popup');
-let bck = document.getElementById('overlay');
-function openPopup() {
-  btn.classList.add('open-popup');
-  bck.style.display = 'block';
-}
-function closePopup() {
-  btn.classList.remove('open-popup');
-}
-let lis1 = document.getElementById('idul');
+// let bck = document.getElementById('overlay');
 
-let lis = lis1.getElementsByClassName('idli');
+const openPopup = () => {
+  btn.classList.add('open-popup');
+  // bck.style.display = 'block';
+};
+
+const closePopup = () => {
+  btn.classList.remove('open-popup');
+};
+
+let lis = document.getElementsByClassName('idli');
+document.querySelectorAll('.idli').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    let part = parseInt(item.value);
+    changbr(part);
+  });
+});
 
 function changbr(p) {
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].style.backgroundColor = 'white';
-  }
-  lis[p].style.backgroundColor = '#5eb6e1';
+  Array.from(lis).map((l) => {
+    l.classList.remove('bg-blue');
+  });
+
+  lis[p].classList.add('bg-blue');
 }
+$(function () {
+  $('#testmonialid').slick({
+    slidesToShow: 3,
+    // initialSlide: 2,
+    centerMode: true,
+    centerPadding: '53px',
+    arrows: true,
+    dots: false,
+    infinite: true,
+    cssEase: 'linear',
+  });
+});
